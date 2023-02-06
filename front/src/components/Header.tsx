76693437a0d8bcfee34722a14f1788800eb0ec12
nakashi94 <n0904yBUNJOU@gmail.com>
2023-02-06 14:09:39 +0900

    fix: warning and firebase signin

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -3,21 +3,47 @@ import Router from "next/router";
 import React, { memo } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { app } from "../config/firebase/firebaseInit";
+import { provider } from "../config/firebase/auth-google-provider-create";
 
 export const Header: React.FC = memo(() => {
   const onClickLoginButton = () => {
-    Router.push("/guest");
-    toast.success("Login Successful!", {
-      icon: "💪",
-      position: "bottom-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "dark",
-    });
+    const auth = getAuth(app);
+    signInWithPopup(auth, provider)
+      .then(async (result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user);
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+        await Router.push("/mypage");
+        toast.success("Login Successful!", {
+          icon: "💪",
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
   };
 
   return (
---
+++ front/src/config/firebase/auth-google-provider-create.js
@@ -0,0 +1,4 @@
+import { GoogleAuthProvider } from "firebase/auth";
+
+export const provider = new GoogleAuthProvider();
+// export const provider = new GoogleAuthProvider().addScope('https://www.googleapis.com/auth/contacts.readonly');
