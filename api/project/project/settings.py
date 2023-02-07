db196482f924b025c8793ec88e90783c8d9588a9
Ry268 <ryotaro545@gmail.com>
2023-02-07 10:02:53 +0900

    secret key change

--- api/project/project/settings.py
+++ api/project/project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m6@sq*5i+e*ejrp!7+)ic2s#ovu9_2ba@9whuq_(-1%1i@ynb4'
+SECRET_KEY = os.urandom(24)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
