b7fc4ebf8a68beb690acb29bd217d2a23ca911f5
Ry268 <ryotaro545@gmail.com>
2023-02-12 01:05:14 +0900

    "database completion"

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Fooddata
+from .models import Fooddata, Favorite, FoodLog, Profile, User
 # Register your models here.
 
 admin.site.register(Fooddata)
+admin.site.register(Favorite)
+admin.site.register(FoodLog)
+admin.site.register(Profile)
+admin.site.register(User)
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,33 +1,106 @@
 from django.db import models
 
 
+class Favorite(models.Model):
+    id = models.UUIDField(primary_key=True)
+    user = models.ForeignKey('User', models.DO_NOTHING)
+    food_name = models.TextField()
+    kcal = models.FloatField(blank=True, null=True)
+    water = models.FloatField(blank=True, null=True)
+    protein = models.FloatField(blank=True, null=True)
+    fat = models.FloatField(blank=True, null=True)
+    carbo = models.FloatField(blank=True, null=True)
+    ile = models.FloatField(blank=True, null=True)
+    leu = models.FloatField(blank=True, null=True)
+    lys = models.FloatField(blank=True, null=True)
+    met = models.FloatField(blank=True, null=True)
+    cys = models.FloatField(blank=True, null=True)
+    phe = models.FloatField(blank=True, null=True)
+    tyr = models.FloatField(blank=True, null=True)
+    thr = models.FloatField(blank=True, null=True)
+    trp = models.FloatField(blank=True, null=True)
+    val = models.FloatField(blank=True, null=True)
+    his = models.FloatField(blank=True, null=True)
+    arg = models.FloatField(blank=True, null=True)
+    asp = models.FloatField(blank=True, null=True)
+    glu = models.FloatField(blank=True, null=True)
+    gly = models.FloatField(blank=True, null=True)
+    pro = models.FloatField(blank=True, null=True)
+    ser = models.FloatField(blank=True, null=True)
+    hyp = models.FloatField(blank=True, null=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'favorite'
+
+
+class FoodLog(models.Model):
+    id = models.UUIDField(primary_key=True)
+    user = models.ForeignKey('User', models.DO_NOTHING)
+    food_name = models.TextField()
+    amount = models.FloatField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'food_log'
+
+
 class Fooddata(models.Model):
     food_number = models.CharField(primary_key=True, max_length=5)
     food_name = models.TextField()
-    kcal = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    water = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    protein = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    fat = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    carbo = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    ile = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    leu = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    lys = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    met = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    cys = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    phe = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    tyr = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    thr = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    trp = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    val = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    his = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    arg = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    asp = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    glu = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    gly = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    pro = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    ser = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-    hyp = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    kcal = models.FloatField(blank=True, null=True)
+    water = models.FloatField(blank=True, null=True)
+    protein = models.FloatField(blank=True, null=True)
+    fat = models.FloatField(blank=True, null=True)
+    carbo = models.FloatField(blank=True, null=True)
+    ile = models.FloatField(blank=True, null=True)
+    leu = models.FloatField(blank=True, null=True)
+    lys = models.FloatField(blank=True, null=True)
+    met = models.FloatField(blank=True, null=True)
+    cys = models.FloatField(blank=True, null=True)
+    phe = models.FloatField(blank=True, null=True)
+    tyr = models.FloatField(blank=True, null=True)
+    thr = models.FloatField(blank=True, null=True)
+    trp = models.FloatField(blank=True, null=True)
+    val = models.FloatField(blank=True, null=True)
+    his = models.FloatField(blank=True, null=True)
+    arg = models.FloatField(blank=True, null=True)
+    ala = models.FloatField(blank=True, null=True)
+    asp = models.FloatField(blank=True, null=True)
+    glu = models.FloatField(blank=True, null=True)
+    gly = models.FloatField(blank=True, null=True)
+    pro = models.FloatField(blank=True, null=True)
+    ser = models.FloatField(blank=True, null=True)
+    hyp = models.FloatField(blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'fooddata'
+
+
+class Profile(models.Model):
+    id = models.UUIDField(primary_key=True)
+    user = models.ForeignKey('User', models.DO_NOTHING)
+    height = models.FloatField(blank=True, null=True)
+    weight = models.FloatField(blank=True, null=True)
+    created_at = models.DateTimeField()
+    update_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'profile'
+
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True)
+    email = models.TextField()
+    remember_token = models.TextField()
+    created_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'user'
